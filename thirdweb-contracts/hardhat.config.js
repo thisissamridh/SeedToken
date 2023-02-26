a741c42ac026de2d30079990709a4a4a33792910
thisissamridh <samridhsinghind@gmail.com>
2023-02-27 02:57:43 +0530

    creating pages

--- thirdweb-contracts/hardhat.config.js
+++ thirdweb-contracts/hardhat.config.js
@@ -5,8 +5,10 @@ module.exports = {
     defaultNetwork: 'mumbai',
     networks: {
       mumbai: {
-        url:'https://polygon-mumbai.g.alchemy.com/v2/Ch4l3o1qXehh7z77F_tFYFKO-IAtU0i8',
-        accounts: [`0x${process.env.PRIVATE_KEY}`], 
+        url: 'https://polygon-mumbai.g.alchemy.com/v2/Ch4l3o1qXehh7z77F_tFYFKO-IAtU0i8',
+        accounts: [`0x${process.env.PRIVATE_KEY}`],
+      },
+    },
     settings: {
       optimizer: {
         enabled: true,
